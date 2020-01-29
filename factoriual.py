0c7866618a9cee3c872acf927797e8e374a702ae
hari371 <60421293+hari371@users.noreply.github.com>
2020-01-29 12:40:18 +0530

    Add files via upload

---
+++ factoriual.py
@@ -0,0 +1,7 @@
+x=5
+r=1
+while x!=0:
+  r=r*x
+  x=x-1
+print(r)
+  
---
+++ recurse.py
@@ -0,0 +1,9 @@
+def fact(x):
+    if x==1:
+        return 1
+    else:
+        return x*fact(x-1)
+
+y=fact(5)
+print(int(y))
+  
